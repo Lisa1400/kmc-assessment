6e4261a0ea2e97153a4de2a3d946e795e8f3552d
Lisa1400 <lisamabe631@gmail.com>
2021-07-06 08:24:46 +0200

    imported classes

---
+++ main.js
@@ -0,0 +1,5 @@
+const Orbit = require('./objects/orbit');
+const Vehicle = require('./objects/vehicle');
+const  Velocity  = require('./objects/velocity');
+const Weather = require('./objects/weather');
+const TravelOptimised = require('./objects/travelOptimise');
