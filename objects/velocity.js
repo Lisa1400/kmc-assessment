0421555fea749a2372652b7f0c993ba981689463
Lisa1400 <lisamabe631@gmail.com>
2021-07-06 07:59:17 +0200

    created velocity class

---
+++ objects/velocity.js
@@ -0,0 +1,22 @@
+class Velocity {
+  constructor(speed, unit) {
+    this.speed = speed;
+    this.unit = unit;
+  }
+
+  getSpeed() {
+    return this.speed;
+  }
+
+  setSpeed(speed) {
+    this.speed = speed;
+  }
+
+  getUnit() {
+    return this.unit;
+  }
+
+  setUnit() {
+    this.unit = newUnit;
+  }
+}
