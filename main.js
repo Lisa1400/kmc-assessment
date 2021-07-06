18cb56c117289b293fe9f14f1ed2171edef8cdc2
Lisa1400 <lisamabe631@gmail.com>
2021-07-06 08:35:38 +0200

    created a function which gets all vehicles

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const Orbit = require('./objects/orbit');
-const Vehicle = require('./objects/vehicle');
-const  Velocity  = require('./objects/velocity');
-const Weather = require('./objects/weather');
-const TravelOptimised = require('./objects/travelOptimise');
+const Orbit = require("./objects/orbit");
+const Vehicle = require("./objects/vehicle");
+const Velocity = require("./objects/velocity");
+const Weather = require("./objects/weather");
+const TravelOptimised = require("./objects/travelOptimise");
+
+function getAllVehicle() {
+  let vehicles = [];
+
+  vehicles.push(new Vehicle("bike", new Velocity(10, "megamiles/hour"), 2));
+  vehicles.push(new Vehicle("tuktuk", new Velocity(12, "megamiles/hour"), 1));
+  vehicles.push(new Vehicle("car", new Velocity(20, "megamiles/hour"), 3));
+
+  return vehicles;
+}
