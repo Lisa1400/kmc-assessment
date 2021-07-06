94d102fb4429a92b72e6f8981aaee6e0ea043a2e
Lisa1400 <lisamabe631@gmail.com>
2021-07-06 08:18:37 +0200

    created weather class

---
+++ objects/weather.js
@@ -0,0 +1,31 @@
+class Weather {
+  constructor(weatherType, craterChangeRate, supportedVehicles) {
+    this.weatherType = weatherType;
+    this.craterChangeRate = craterChangeRate;
+    this.supportedVehicles = supportedVehicles;
+  }
+
+  getWeatherType() {
+    return this.weatherType;
+  }
+
+  setWeatherType(newWeatherType) {
+    this.weatherType = newWeatherType;
+  }
+
+  getCraterChangeRate() {
+    return this.craterChangeRate;
+  }
+
+  setCraterChangeRate(newCraterChangeRate) {
+    this.craterChangeRate = newCraterChangeRate;
+  }
+
+  getSupportedVehicles() {
+    return this.supportedVehicles;
+  }
+
+  setSupportedVehicles(newSupportedVehicles) {
+    this.supportedVehicles = newSupportedVehicles;
+  }
+}
