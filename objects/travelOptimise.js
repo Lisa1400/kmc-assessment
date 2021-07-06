54951f0360afd906cc78106949b6e2239bd5f7b7
Lisa1400 <lisamabe631@gmail.com>
2021-07-06 07:50:44 +0200

    created a class that optimises travel

---
+++ objects/travelOptimise.js
@@ -0,0 +1,31 @@
+class TravelOptimised {
+  constructor(timeTravelled, orbit, vehicle) {
+    this.timeTravelled = timeTravelled;
+    this.orbit = orbit;
+    this.vehicle;
+  }
+
+  getTimeTravelled() {
+    return this.timeTravelled;
+  }
+
+  setTimeTravelled(newTimeTravelled) {
+    this.timeTravelled = newTimeTravelled;
+  }
+
+  getOrbit() {
+    return this.orbit;
+  }
+
+  setOrbit(newOrbit) {
+    this.orbit = newOrbit;
+  }
+
+  getVehicle() {
+    return this.vehicle;
+  }
+
+  setVehicle(newVehicle) {
+    this.vehicle = newVehicle;
+  }
+}
